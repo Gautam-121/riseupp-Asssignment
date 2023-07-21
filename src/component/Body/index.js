1574739d4ac572652f9f180c0749d5328f49f6cf
Gautam-121 <gautamdhakate1234@gmail.com>
2023-07-21 10:22:48 +0530

    app logo and Search Ui is Done

--- src/component/Body/index.js
+++ src/component/Body/index.js
@@ -1,8 +1,26 @@
+import "./body.css"
+import Logo from "../../assets/app_logo.png"
+
 const Body = ()=>{
     return(
-        <div className="max-width">
-            <h3>Icons</h3>
-            <h3>Search bar</h3>
+        <div className="app_layout_wrapper">
+           <div className="logo_wrapper">
+             <img 
+              src={Logo} 
+              alt="This is logo"
+              className="main_logo" 
+            />
+           </div>
+            <div className="search_container">
+                <div className="search_btn_wrapper">
+                <input 
+                type="text" 
+                placeholder="Search Images"
+                className="search_input"
+                />
+                <button className="search_btn cur-poi">Search</button>
+                </div>
+            </div>
             <h3>Filter</h3>
             <h3>Cart</h3>
         </div>
