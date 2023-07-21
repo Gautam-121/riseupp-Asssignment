adaa1e3f3994dceb444d2525f2b1cb25de85fbda
Gautam-121 <gautamdhakate1234@gmail.com>
2023-07-21 09:29:20 +0530

    craete static bonacular structure planning how the look

---
+++ src/component/Body/index.js
@@ -0,0 +1,12 @@
+const Body = ()=>{
+    return(
+        <div className="max-width">
+            <h3>Icons</h3>
+            <h3>Search bar</h3>
+            <h3>Filter</h3>
+            <h3>Cart</h3>
+        </div>
+    )
+}
+
+export default Body
